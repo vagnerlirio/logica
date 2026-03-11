64617b6843c0c7c3204ba64c3846d4cedd4c64cc
VAGNER DA PAIXAO LIRIO <vagner.lirio@aluno.senac.rio>
2026-03-11 20:39:51 -0300

    prog2 ayualizado novamente

--- prog2.js
+++ prog2.js
@@ -1,2 +1,7 @@
 let valor = 8;
 console.log("O conteúdo do valor é: " + valor);  
+
+/*
+agora adicione este arquivo ao seu git
+depois faça um commit sinalizando o que você fez
+envie para o github*/
